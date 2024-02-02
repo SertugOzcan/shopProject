ddf7d45aff8f5248b89824732a230709c81ab56c
LENOVO <sertugozcan@gmail.com>
2024-02-02 19:56:36 +0300

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,39 @@
 /* eslint-disable no-unused-vars */
 
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import ShoppingCart from './ShoppingCart';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import ShoppingCart from "./ShoppingCart";
 
 function Navbar() {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const cartProducts = useSelector((state) => state.cartProducts);
 
   const toggleCart = () => {
     setIsCartOpen(!isCartOpen);
   };
 
+  const itemCount = cartProducts.reduce(
+    (total, product) => total + product.amount,
+    0
+  );
+
   return (
     <nav className="navbar">
       <ul>
-        <li className='anasayfa'><Link to="/">Anasayfa</Link></li>
-        <li><Link to="/products">Ürünler</Link></li>
+        <li className="anasayfa">
+          <Link to="/">Anasayfa</Link>
+        </li>
+        <li>
+          <Link to="/products">Ürünler</Link>
+        </li>
       </ul>
-      
+      {/* Sepet sayacı */}
+      <div className="cart-counter">
+        <span>{itemCount}</span>
+      </div>
       <div className="shopping-cart-icon" onClick={toggleCart}>
         <FontAwesomeIcon icon={faShoppingCart} />
       </div>
